eb63d4197d1a501215eb9969dd848e84755cba97
Rachel Farley <rachyl.farley@gmail.com>
2021-03-27 13:47:54 -0600

    create dependency chart

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import FlowTest from './FlowTest';
 import CustomNodeExample from './TestNode';
 import FlowContainer from './FlowContainer';
+import DependencyChart from './DependencyChart';
 import '../styles.scss';
 
+
 export default function App() {
+
   return (
     <div>
-      <FlowContainer />
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={DependencyChart} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
