e136645ce734b9379ec1a53a92f31015c2b71b5f
Angelo <quailangelo@gmail.com>
2021-03-31 11:00:00 -0400

    easier dev experience

--- src/contexts/AppsProvider.js
+++ src/contexts/AppsProvider.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import AppsContext from './AppsContext';
-import appData from '../dummy-data';
+import appData from './ATRIOM';
 import colors from '../helpers/colors';
 import { createColorMap, convertAppObj } from '../helpers';
 
 export default ({ children }) => {
   const [apps, setApps] = useState([]);
 
-  // useEffect(() => {
-  //   const colorMap = createColorMap(appData, colors);
-  //   const convertedApps = convertAppObj(appData, colorMap);
-  //   setApps(convertedApps);
-  // }, []);
+  useEffect(() => {
+    console.log(appData);
+    // const contents = JSON.parse(appData);
+    // // const contents = eval(e.target.result);
+    const colorMap = createColorMap(appData, colors);
+    const convertedApps = convertAppObj(appData, colorMap);
+    setApps(convertedApps);
+  }, []);
 
   const { Provider } = AppsContext;
   return <Provider value={{ apps, setApps }}>{children}</Provider>;
