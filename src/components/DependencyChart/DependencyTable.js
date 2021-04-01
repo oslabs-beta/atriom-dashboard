7ab8dcff598ff478cdf9770274c88df399e30cfc
Rachel Farley <rachyl.farley@gmail.com>
2021-04-01 09:53:19 -0600

    table styles

--- src/components/DependencyChart/DependencyTable.js
+++ src/components/DependencyChart/DependencyTable.js
@@ -1,46 +1,53 @@
 import React from 'react';
 import { useTable } from 'react-table';
-import MaUTable from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
+import MaUTable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import styles from '../../styles/DependencyChart.scss';
 
-
-export default function Table({columns, data}) {
+export default function Table({ columns, data }) {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
-    prepareRow
+    prepareRow,
   } = useTable({
-    columns, data
+    columns,
+    data,
   });
 
   return (
     <MaUTable {...getTableProps()}>
       <TableHead>
-        {headerGroups.map(headerGroup => (
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <TableCell {...column.getHeaderProps()}>{column.render("Header")}</TableCell>
+        {headerGroups.map((headerGroup) => (
+          <TableRow {...headerGroup.getHeaderGroupProps()} className="row">
+            {headerGroup.headers.map((column) => (
+              <TableCell {...column.getHeaderProps()} className="headerCell">
+                {column.render('Header')}
+              </TableCell>
             ))}
           </TableRow>
         ))}
       </TableHead>
-      <TableBody {...getTableBodyProps()}>
+      <TableBody {...getTableBodyProps()} className="body">
         {rows.map((row, i) => {
           prepareRow(row);
           return (
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <TableCell {...cell.getCellProps()}>{cell.render("Cell")}</TableCell>;
+            <TableRow {...row.getRowProps()} className="row">
+              {row.cells.map((cell) => {
+                return (
+                  <TableCell {...cell.getCellProps()} className="cell">
+                    {cell.render('Cell')}
+                  </TableCell>
+                );
               })}
             </TableRow>
           );
         })}
       </TableBody>
     </MaUTable>
-  )
-};
+  );
+}
