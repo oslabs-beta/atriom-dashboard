fa4bece3baec33ee890b0f8da8baa419f1c8c285
rachyl6787 <37519153+rachyl6787@users.noreply.github.com>
2021-04-09 17:02:08 -0600

    Update UserModel.js

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const PG_URI =
   process.env.NODE_ENV === 'testing'
     ? process.env.PG_URI_TEST
-    : process.env.PG_URI_DEV;
+    : process.env.PG_URI;
 
 const pool = new Pool({
   connectionString: PG_URI,
