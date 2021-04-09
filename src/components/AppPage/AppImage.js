a265346b21fa8eab371c7a2044a577f2b31e5220
Maureen Onchiri <64867546+onchirim@users.noreply.github.com>
2021-04-09 09:39:32 -0500

    Delete AppImage.js

--- src/components/AppPage/AppImage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from '../images/logo.png';
-
-const AppImage = () => {
-  return <img src={logo} style={{ height: '50px', width: '50px' }} />;
-};
-
-export default AppImage;
