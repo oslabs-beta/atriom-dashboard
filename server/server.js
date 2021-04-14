cc6ca01dfd19b775f1486c9e1542821ff11badb9
Angelo <quailangelo@gmail.com>
2021-04-14 13:47:11 -0400

    added basic server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,36 @@
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const appsDb = require('./db/apps');
-// const App = require('./models/AppModel');
-const User = require('./models/UserModel');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require('./routes/auth');
+// const authRouter = require('./routes/auth');
 
-app.use('/auth', authRouter);
+// app.use('/auth', authRouter);
+if (process.env.NODE_ENV === 'prod') {
+  app.use('/build', express.static(path.join(__dirname, '../build/')));
+
+  app.get('*', (req, res) =>
+    res.status(200).sendFile(path.join(__dirname, '../index.html'))
+  );
+}
+
+app.get('*', (req, res) => {
+  return res.status(404).json();
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: 'An error occurred',
+  };
+  const error = { ...defaultErr, ...err };
+  return res.status(error.status).json(error.message);
+});
 
 module.exports = app.listen(port, () =>
   console.log(`Listening on port ${port}`)
--- src/components/DropZone/DropZoneContainer.js
+++ src/components/DropZone/DropZoneContainer.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import DropZone from './DropZone';
 import '../../styles/DropZoneContainer.scss';
+import logo from '../../images/colorLogo.svg';
 
 function DropZoneContainer({ history }) {
   return (
     <div className="content">
       {/* <h1 className="title">ATRIOM</h1> */}
-      <img
-        src='../src/images/colorLogo.svg'
-        alt="Atriom logo"
-        height="150vh"
-        id="DZLogo"
-      />
+      <img src={logo} alt="Atriom logo" height="150vh" id="DZLogo" />
       <DropZone history={history} />
     </div>
   );
