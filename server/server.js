a4ed2369bfc8c203ac66cbab1ab16c5936e98808
onchirim <64867546+onchirim@users.noreply.github.com>
2021-04-18 12:11:53 -0500

    new-tests passing

--- server/server.js
+++ server/server.js
@@ -33,3 +33,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {});
+
+module.exports = app;
--- tests/unit/components/Navbar/DropMenu.test.js
+++ tests/unit/components/Navbar/DropMenu.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-17-updated';
-import toJson from 'enzyme-to-json';
-
 import DropMenu from '../../../../src/components/NavBar/Search';
-import { ClickAwayListener } from '@material-ui/core';
 
 configure({ adapter: new Adapter() });
 
@@ -13,10 +10,6 @@ describe('React unit tests', () => {
     let wrapper;
     beforeAll(() => {
       wrapper = shallow(<DropMenu />);
-    });
-
-    xit('Verifies that DropMenu returns ClickAwayListener component', () => {
-      expect(wrapper.contains(<ClickAwayListener />));
     });
 
     it("Verifies that the DropMenu component has a className 'search'", () => {
